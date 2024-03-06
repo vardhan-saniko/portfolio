7afc8e3e477cfc900a4ecc98bf76bc0f239ee2e8
vardhan-saniko <90535201+vardhan-saniko@users.noreply.github.com>
2024-03-06 12:11:17 -0500

    Update portfolio.ts

--- portfolio.ts
+++ portfolio.ts
@@ -17,7 +17,7 @@ export const greetings: GreetingsType = {
   description:
     "I'm passionate Full Stack web developer having an experience of web applications with Python, Django, React.js, Ruby and Ruby on Rails",
   resumeLink:
-    'https://drive.google.com/file/d/1R_v65TE8OG31a6QXGjVGDM9AWxB110zi/view?usp=sharing',
+    'https://drive.google.com/file/d/1bjjstOLoHUVH-1Azw2wFISSEhO_j7Gdw/view?usp=sharing',
 };
 
 export const openSource = {
